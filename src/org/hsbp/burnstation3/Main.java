4d1767d1b223297161f2be29d38e6aa28a1cec68
András Veres-Szentkirályi <vsza@vsza.hu>
2013-07-29 19:50:04 +0200

    added albums list filler skeleton

--- src/org/hsbp/burnstation3/Main.java
+++ src/org/hsbp/burnstation3/Main.java
@@ -2,7 +2,9 @@ package org.hsbp.burnstation3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.widget.*;
+import java.util.*;
 
 public class Main extends Activity
 {
@@ -13,11 +15,34 @@ public class Main extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         String[] test = {"foo", "bar", "baz"};
-        int[] widgets = {R.id.albums, R.id.tracks, R.id.playlist};
+        int[] widgets = {R.id.tracks, R.id.playlist};
         for (int widget : widgets) {
             ListView lv = (ListView)findViewById(widget);
             lv.setAdapter(new ArrayAdapter<String>(
                         this, android.R.layout.simple_list_item_1, test));
+        }
+        new AlbumListFillTask().execute();
+    }
+
+    private class AlbumListFillTask extends AsyncTask<Void, Void, List<? extends Map<String, ?>>> {
+        @Override
+        protected List<? extends Map<String, ?>> doInBackground(Void... ignored) {
+            List<Map<String, String>> albums = new ArrayList<Map<String, String>>();
+            // TODO download into albums
+            Map<String, String> album = new HashMap<String, String>();
+            album.put("name", "Test album title");
+            album.put("artist", "Test album artist");
+            albums.add(album);
+            return albums;
+        }
+
+        @Override
+        protected void onPostExecute(List<? extends Map<String, ?>> result) {
+            ListView lv = (ListView)findViewById(R.id.albums);
+            final String[] map_from = {"name", "artist"};
+            final int[] map_to = {R.id.album_name, R.id.album_artist};
+            lv.setAdapter(new SimpleAdapter(Main.this, result,
+                        R.layout.albums_item, map_from, map_to));
         }
     }
 }
