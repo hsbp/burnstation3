ed7a6ca6f25b2eafaa5e8cbe81ff82ce731c6e5c
András Veres-Szentkirályi <vsza@vsza.hu>
2014-04-05 14:41:05 +0200

    initialize audio member missed in 95f7a35

--- src/org/hsbp/burnstation3/Track.java
+++ src/org/hsbp/burnstation3/Track.java
@@ -15,7 +15,7 @@ import org.json.JSONException;
 public class Track implements Runnable, API.Notifiable {
     public final String artistName, name, id;
     protected final File localFile;
-    protected URL audio;
+    protected final URL audio;
     public final int duration;
     protected final static Set<String> beingCached =
         Collections.synchronizedSet(new HashSet<String>());
@@ -33,10 +33,11 @@ public class Track implements Runnable, API.Notifiable {
     protected final Set<Notifiable> subscribers = new HashSet<Notifiable>();
     protected PlayerUI ui;
 
-    protected Track(String artistName, String name, String id, File localFile, int duration) {
+    protected Track(String artistName, String name, String id, URL audio, File localFile, int duration) {
         this.artistName = artistName;
         this.name = name;
         this.id = id;
+        this.audio = audio;
         this.localFile = localFile;
         this.duration = duration;
     }
@@ -46,7 +47,8 @@ public class Track implements Runnable, API.Notifiable {
         File cacheDir = new File(ctx.getCacheDir(), CACHE_DIR);
         cacheDir.mkdirs();
         final String trackId = obj.getString(ID);
-        return new Track(obj.getString(ARTIST_NAME), obj.getString(NAME), trackId,
+        return new Track(obj.getString(ARTIST_NAME), obj.getString(NAME),
+                trackId, new URL(obj.getString(AUDIO)),
                 new File(cacheDir, trackId + FILE_SUFFIX), obj.getInt(DURATION));
     }
 
